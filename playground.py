cf842539198333cf0ae00bf944b9171573936741
Stephen Impraim <kofibondze@gmail.com>
2025-02-02 16:06:11 -0500

    Playground code

---
+++ playground.py
@@ -0,0 +1,9 @@
+'''The process of giving a variable data is 
+   called assignment using the assignment 
+   operator '='''
+my_number = 1
+my_string = 'This is a string'
+my_character = 'c'
+my_double = 1.2345
+my_boolean = True
+my_list = ['a', 1, 5, "Lists can have multiple data types"]
