0157ed812f4191e8502627d7ce4c1d5833cbe72f
Stephen Impraim <kofibondze@gmail.com>
2025-02-12 17:35:37 -0500

    Adding boolean operations

---
+++ booleanoperations.py
@@ -0,0 +1,8 @@
+a > b #Greater Than
+a < b #Less than
+a == b #Equals
+a != b #Does not equal
+a =< b #Less than or equal to
+a => b #Greater than or equal to
+a and b #AND
+a or b #OR
--- hw1.py
+++ hw1.py
@@ -1,4 +1,7 @@
 #Ask for input. This input will come in as a string
+your_age = input("What is your age: ")
+your_age = your_age + 37
+print("Your name is " + str(your_age))
 #Convert that string to a number
 #Add 37 to that number
 #Print the results (might need to convert again)
