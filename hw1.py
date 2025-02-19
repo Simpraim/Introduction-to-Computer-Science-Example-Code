3d1acf5a94be907b923c9069e81246093102772b
Stephen Impraim <kofibondze@gmail.com>
2025-02-19 15:54:46 -0500

    added more context

--- hw1.py
+++ hw1.py
@@ -1,4 +1,4 @@
-#Ask for input. This input will come in as a string
+#Ask for input and store in a variable. This input will come in as a string
 #Convert that string to a number
 #Add 37 to that number
 #Print the results (might need to convert again)
