f1aca8d3dddd932c21d520c368b8371f9085797f
Stephen Impraim <kofibondze@gmail.com>
2025-02-23 22:33:26 -0500

    Adding while loops

---
+++ while.py
@@ -0,0 +1,8 @@
+#Let's create a counter that counts to 5
+a = 1
+while a < 5: #While a is less than 5, do something...
+ print(a)  #This is the something
+ a = a + 1 #This changes the value of a so that you're not stuck in the loop
+#Line 4 will execute, then line 5, then the interpreter will go to line 3
+# with the new value of a (2 for example) and check if it is less than 5
+# If a is NOT less than 5, lines 4 and 5 will not execute
